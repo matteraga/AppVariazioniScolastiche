fede5d809ac948004fd6c1db4a9f97b9bcc44bd2
matteraga <matteoraganini@gmail.com>
2023-12-27 11:42:50 +0100

    Merge receivers

---
+++ app/src/main/java/me/matteraga/appvariazioniscolastiche/alarmmanager/RescheduleAlarmReceiver.kt
@@ -0,0 +1,27 @@
+package me.matteraga.appvariazioniscolastiche.alarmmanager
+
+import android.app.AlarmManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+class RescheduleAlarmReceiver : BroadcastReceiver() {
+
+    private val actions = listOf(
+        Intent.ACTION_BOOT_COMPLETED,
+        Intent.ACTION_TIME_CHANGED,
+        Intent.ACTION_TIMEZONE_CHANGED
+    ).also {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            it.plus(AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (actions.contains(intent.action)) {
+            AlarmScheduler(context).scheduleSaved()
+        }
+    }
+}
